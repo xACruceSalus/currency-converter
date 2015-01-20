db5af6e330ba547d995dffc6457536b083b12e54
Zack Cooper <zackjcooper@gmail.com>
2015-01-20 12:57:45 -0500

    initial build of documents

---
+++ currency_converter.py
@@ -0,0 +1,8 @@
+def convert(rates, value, from, to):
+    """This function is designed to take in the below arguments and return
+    the value in the specified currency.
+    rates = a list of rates
+    value = a number for which you'd like to exchange for
+    from = a string saying what currency it's in
+    to = a string saying what currency you'd like it in."""
+    
---
+++ test_currency_converter.py
@@ -0,0 +1,5 @@
+from currency_converter import convert
+
+
+def test_convert():
+  assert convert(rates, value, from, to) == 
